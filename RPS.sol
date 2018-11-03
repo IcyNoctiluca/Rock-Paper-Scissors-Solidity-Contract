a74ea4e7bb21ef82d8c66a41875600dd75924a90
IcyNoctiluca <liamperston@hotmail.com>
2018-11-03 22:45:07 +0000

    better documentation added

--- RPS.sol
+++ RPS.sol
@@ -1,26 +1,25 @@
 pragma solidity ^0.4.24
-// author Liam Perston
 
 contract RPS {
 
-  string[] public permittedHands;
-  mapping (string => mapping(string => uint256)) public outcomeMatrix;
+  // to map combinations of hands to the winner
+  mapping (string => mapping(string => uint256)) outcomeMatrix;
 
   uint256 public initialStake;    // cannot be negative
-  uint256 public backingStake;
+  uint256 backingStake;
+
   address player1;
   address player2;
+
   string player1Hand;
   string player2Hand;
-  int winner;
+
+  int winner;       // -1 initally, 0 for draw, 1 for player1, 2 for p2
 
 
   constructor() public {
 
-    permittedHands.push("ROCK");
-    permittedHands.push("PAPER");
-    permittedHands.push("SCISSORS");
-
+    // initialise combinations
     outcomeMatrix["ROCK"]["ROCK"] = 0;
     outcomeMatrix["ROCK"]["PAPER"] = 2;
     outcomeMatrix["ROCK"]["SCISSORS"] = 1;
@@ -31,6 +30,7 @@ contract RPS {
     outcomeMatrix["SCISSORS"]["PAPER"] = 1;
     outcomeMatrix["SCISSORS"]["SCISSORS"] = 0;
 
+    // initialise other variables
     initialStake = 0;
     backingStake = 0;
     player1Hand = "";
@@ -40,14 +40,17 @@ contract RPS {
   }
 
   function initialiseStake() payable public {
-    require(initialStake == 0);
+    require(initialStake == 0 && msg.value > 0);
     initialStake = msg.value;
     player1 = msg.sender;
-
   }
 
   function acceptStake() payable public {
-    require(initialStake != 0 && msg.value == initialStake && backingStake == 0);   // cant be called twice
+    // require an stake to be initialised before it can be accepted
+    // require same amount of ether as init awardStakes
+    // cannot be called twice in a row
+    require(initialStake != 0 && initialStake == msg.value && backingStake == 0);
+
     player2 = msg.sender;
     backingStake = msg.value;
   }
@@ -55,32 +58,40 @@ contract RPS {
 
   function setPlayer1Hand(string hand) public {
     require(msg.sender == player1);
-    require(player1Hand == "");         // cant change hand after other was set
-    require(hand == permittedHands[0] || hand == permittedHands[1] || hand == permittedHands[2]);
+    require(player1Hand == "");         // cannot change hand after already setting it
+    require(hand == "ROCK" || hand == "PAPER" || hand == "SCISSORS");
     player1Hand = hand;
   }
 
   function setPlayer2Hand(string hand) public {
     require(msg.sender == player2);
-    require(player2Hand == "");
-    require(hand == permittedHands[0] || hand == permittedHands[1] || hand == permittedHands[2]);
+    require(player2Hand == "");         // cannot change hand after already setting it
+    require(hand == "ROCK" || hand == "PAPER" || hand == "SCISSORS");
     player2Hand = hand;
   }
 
   function revealHands() public returns mapping(string => string) {
-    require(player1Hand != "" && player2Hand != "");
-    require(winner == -1);
-    mapping handsCommitted(string => string);
+
+    // either p1 or p2 can choose to reveal both hands
+    require(msg.sender == player1 || msg.sender == player2);
+    require(player1Hand != "" && player2Hand != "");          // but both hands must have been committed
+    require(winner == -1);      // require winner not to already exist
+
+    winner = outcomeMatrix[player1Hand][player2Hand];
+
+    mapping (string => string) handsCommitted;
     handsCommitted["player1"] = player1Hand;
     handsCommitted["player2"] = player2Hand;
-    winner = outcomeMatrix[player1Hand][player2Hand];
 
     return handsCommitted;
   }
 
   function awardStakes() public {
-    require(winner != -1);
 
+    require(msg.sender == player1 || msg.sender == player2);
+    require(winner != -1);    // require winner to have been determined by revealing of hands
+
+    // send ether to appropriate players
     if (winner == 1) {
       player1.transfer(initialStake + backingStake);
     } else if (winner == 2) {
@@ -93,11 +104,15 @@ contract RPS {
     reset();
   }
 
+  // tear down all values, reset so game can be played again
+  // cannot be called publicly, only once a game is over
   function reset() {
     initialStake = 0;
     backingStake = 0;
     player1Hand = "";
     player2Hand = "";
+    player1 = address(0)
+    player2 = address(0)
     winner = -1;
   }
 
